f6d108bd0a94a6885a21e664a315dea33454f0cc
xiaojie <z2592027196@gmail.com>
2023-03-23 11:09:45 +0800

    A editor the script.js file,and B commited after commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log('hello world!');
 function bugMethod(){
   console.log('我现在回来修复了代码,这条代码已经没有问题了');
+  console.log('我是A,我也对该文件进行了修改,修改位置与B一样,但是B先提交了,我也需要提交,这样不知道会不会发生什么问题');
 }
 // .....
 // on dev branch second editor
